3be6e901fc8314849b7eb421d730fd01b7aef733
Victor Okumoto <okumotovictor@gmail.com>
2024-02-26 15:22:08 -0300

    finalização do projeto

--- js/app.js
+++ js/app.js
@@ -4,6 +4,10 @@ function comprar() {
 
     if(tipoIngresso.value == 'pista') {
         comprarPista(quantidade);
+    } else if (tipoIngresso.value == 'superior'){
+        comprarSuperior(quantidade);
+    } else if (tipoIngresso.value == 'inferior') {
+        comprarInferior(quantidade);
     }
 }
 
@@ -15,6 +19,30 @@ function comprarPista(quantidade) {
     } else {
         qtdPista = qtdPista - quantidade;
         document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarSuperior(quantidade) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+
+    if (quantidade > qtdSuperior) {
+        alert('Quantidade de ingressos indisponível');
+    } else {
+        qtdSuperior = qtdSuperior - quantidade;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso!');
+    }
+}
+
+function comprarInferior(quantidade) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    if (quantidade > qtdInferior) {
+        alert('Quantidade de ingressos indisponível');
+    } else {
+        qtdInferior = qtdInferior - quantidade;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
         alert('Compra realizada com sucesso!');
     }
 }
