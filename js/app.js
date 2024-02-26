8d746c46bcb91534754830eaea8d2570dc43d663
Victor Okumoto <okumotovictor@gmail.com>
2024-02-26 15:06:06 -0300

    implementar compra pista

--- js/app.js
+++ js/app.js
@@ -1,7 +1,20 @@
 function comprar() {
     let tipoIngresso = document.getElementById('tipo-ingresso');
-    let quantidade = document.getElementById('qtd');
+    let quantidade = parseInt(document.getElementById('qtd').value);
 
-    alert(tipoIngresso.value);
-    alert(quantidade.value);
+    if(tipoIngresso.value == 'pista') {
+        comprarPista(quantidade);
+    }
+}
+
+function comprarPista(quantidade) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+
+    if (quantidade > qtdPista) {
+        alert('Quantidade de ingressos indisponível.');
+    } else {
+        qtdPista = qtdPista - quantidade;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
 }
