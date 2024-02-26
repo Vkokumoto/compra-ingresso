96b126f9d56705066f6350d6b70c4b91d1582853
Victor Okumoto <okumotovictor@gmail.com>
2024-02-26 14:44:07 -0300

    implementar função comprar

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function comprar() {
+    let tipoIngresso = document.getElementById('tipo-ingresso');
+    let quantidade = document.getElementById('qtd');
+
+    alert(tipoIngresso.value);
+    alert(quantidade.value);
+}
